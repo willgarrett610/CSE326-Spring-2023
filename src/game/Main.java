99292c27fdbb318796f941a4dff4aeec3eac00e6
Will Garrett <willgarrett610@gmail.com>
2023-02-16 00:14:30 -0700

    Initial commit

---
+++ src/game/Main.java
@@ -0,0 +1,17 @@
+package game;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setSize(800,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Moom Doon");
+
+        frame.setVisible(true);
+    }
+
+}
